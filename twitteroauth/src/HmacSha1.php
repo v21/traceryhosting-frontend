87b31aa6d668d95604771db45c5d6157b372c01e
George Buckenham <vtwentyone@gmail.com>
2017-10-08 11:04:20 +0100

    update twitteroauth library to 0.7.4

--- twitteroauth/src/HmacSha1.php
+++ twitteroauth/src/HmacSha1.php
@@ -28,9 +28,8 @@ class HmacSha1 extends SignatureMethod
     public function buildSignature(Request $request, Consumer $consumer, Token $token = null)
     {
         $signatureBase = $request->getSignatureBaseString();
-        $request->baseString = $signatureBase;
 
-        $parts = array($consumer->secret, null !== $token ? $token->secret : "");
+        $parts = [$consumer->secret, null !== $token ? $token->secret : ""];
 
         $parts = Util::urlencodeRfc3986($parts);
         $key = implode('&', $parts);
