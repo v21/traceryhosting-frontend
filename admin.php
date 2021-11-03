85e5130d68e0733cc27dd61eb092f31c61a12ec3
George Buckenham <vtwentyone@gmail.com>
2021-11-03 12:37:08 +0000

    lighter borders, procedually colour the IP addresses

--- admin.php
+++ admin.php
@@ -12,6 +12,213 @@ if (!isset($_SESSION['user_id']) || $_SESSION['user_id'] != ADMIN_USER_ID)
   die();
 }
 
+/**
+ * Input: HSL in format Deg, Perc, Perc
+ * Output: An array containing HSL in ranges 0-1
+ * 
+ * Divides $h by 60, and $s and $l by 100.
+ * 
+ * hslToRgb calls this by default.
+*/
+function degPercPercToHsl($h, $s, $l) { 
+  //convert h, s, and l back to the 0-1 range
+
+  //convert the hue's 360 degrees in a circle to 1
+  $h /= 360;
+
+  //convert the saturation and lightness to the 0-1 
+  //range by multiplying by 100
+  $s /= 100;
+  $l /= 100;
+
+  $hsl['h'] =  $h;
+  $hsl['s'] = $s;
+  $hsl['l'] = $l;
+
+  return $hsl;
+}
+
+/**
+ * Converts an HSL hue to it's RGB value.  
+ *
+ * Input: $temp1 and $temp2 - temperary vars based on 
+ * whether the lumanence is less than 0.5, and 
+ * calculated using the saturation and luminence
+ * values.
+ *  $hue - the hue (to be converted to an RGB 
+ * value)  For red, add 1/3 to the hue, green 
+ * leave it alone, and blue you subtract 1/3 
+ * from the hue.
+ *
+ * Output: One RGB value.
+ *
+ * Thanks to Easy RGB for this function (Hue_2_RGB).
+ * http://www.easyrgb.com/index.php?X=MATH&$h=19#text19
+ *
+*/
+function hueToRgb($temp1, $temp2, $hue) {
+  if ($hue < 0) { 
+      $hue += 1;
+  }
+  if ($hue > 1) {
+      $hue -= 1;
+  }
+
+  if ((6 * $hue) < 1 ) {
+      return ($temp1 + ($temp2 - $temp1) * 6 * $hue);
+  } elseif ((2 * $hue) < 1 ) {
+      return $temp2;
+  } elseif ((3 * $hue) < 2 ) {
+      return ($temp1 + ($temp2 - $temp1) * ((2 / 3) - $hue) * 6);
+  }
+  return $temp1;
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/.
+ * Assumes h, s, and l are in the format Degrees,
+ * Percent, Percent, and returns r, g, and b in 
+ * the range [0 - 255].
+ *
+ * Called by hslToHex by default.
+ *
+ * Calls: 
+ *   degPercPercToHsl
+ *   hueToRgb
+ *
+ * @param   Number  h       The hue value
+ * @param   Number  s       The saturation level
+ * @param   Number  l       The luminence
+ * @return  Array           The RGB representation
+ */
+function hslToRgb($h, $s, $l){
+  $hsl = degPercPercToHsl($h, $s, $l);
+  $h = $hsl['h'];
+  $s = $hsl['s'];
+  $l = $hsl['l'];
+
+  //If there's no saturation, the color is a greyscale,
+  //so all three RGB values can be set to the lightness.
+  //(Hue doesn't matter, because it's grey, not color)
+  if ($s == 0) {
+      $r = $l * 255;
+      $g = $l * 255;
+      $b = $l * 255;
+  }
+  else {
+      //calculate some temperary variables to make the 
+      //calculation eaisier.
+      if ($l < 0.5) {
+          $temp2 = $l * (1 + $s);
+      } else {
+          $temp2 = ($l + $s) - ($s * $l);
+      }
+      $temp1 = 2 * $l - $temp2;
+
+      //run the calculated vars through hueToRgb to
+      //calculate the RGB value.  Note that for the Red
+      //value, we add a third (120 degrees), to adjust 
+      //the hue to the correct section of the circle for
+      //red.  Simalarly, for blue, we subtract 1/3.
+      $r = 255 * hueToRgb($temp1, $temp2, $h + (1 / 3));
+      $g = 255 * hueToRgb($temp1, $temp2, $h);
+      $b = 255 * hueToRgb($temp1, $temp2, $h - (1 / 3));
+  }
+
+  $rgb['r'] = $r;
+  $rgb['g'] = $g;
+  $rgb['b'] = $b;
+
+  return $rgb;
+}
+
+/**
+ * Converts HSL to Hex by converting it to 
+ * RGB, then converting that to hex.
+ * 
+ * string hslToHex($h, $s, $l[, $prependPound = true]
+ * 
+ * $h is the Degrees value of the Hue
+ * $s is the Percentage value of the Saturation
+ * $l is the Percentage value of the Lightness
+ * $prependPound is a bool, whether you want a pound 
+ *  sign prepended. (optional - default=true)
+ *
+ * Calls: 
+ *   hslToRgb
+ *
+ * Output: Hex in the format: #00ff88 (with 
+ * pound sign).  Rounded to the nearest whole
+ * number.
+*/
+function hslToHex($h, $s, $l, $prependPound = true) {
+  //convert hsl to rgb
+  $rgb = hslToRgb($h,$s,$l);
+
+  //convert rgb to hex
+  $hexR = $rgb['r'];
+  $hexG = $rgb['g'];
+  $hexB = $rgb['b'];
+
+  //round to the nearest whole number
+  $hexR = round($hexR);
+  $hexG = round($hexG);
+  $hexB = round($hexB);
+
+  //convert to hex
+  $hexR = dechex($hexR);
+  $hexG = dechex($hexG);
+  $hexB = dechex($hexB);
+
+  //check for a non-two string length
+  //if it's 1, we can just prepend a
+  //0, but if it is anything else non-2,
+  //it must return false, as we don't 
+  //know what format it is in.
+  if (strlen($hexR) != 2) {
+      if (strlen($hexR) == 1) {
+          //probably in format #0f4, etc.
+          $hexR = "0" . $hexR;
+      } else {
+          //unknown format
+          return false;
+      }
+  }
+  if (strlen($hexG) != 2) {
+      if (strlen($hexG) == 1) {
+          $hexG = "0" . $hexG;
+      } else {
+          return false;
+      }
+  }
+  if (strlen($hexB) != 2) {
+      if (strlen($hexB) == 1) {
+          $hexB = "0" . $hexB;
+      } else {
+          return false;
+      }
+  }
+
+  //if prependPound is set, will prepend a
+  //# sign to the beginning of the hex code.
+  //(default = true)
+  $hex = "";
+  if ($prependPound) {
+      $hex = "#";
+  }
+
+  $hex = $hex . $hexR . $hexG . $hexB;
+
+  return $hex;
+}
+
+function stringToColor($str) {
+  $hash = md5($str);
+  $hue = hexdec(substr($hash, 0, 3))/hexdec("fff");
+  return hslToHex($hue * 360, 60, 80);
+}
+
 
 ?>
 <!doctype html>
@@ -152,7 +359,7 @@ if (!$include_inactive)
       <td>{$value['frequency']}</td>
       <td><a href=\"admin_single.php?screen_name={$value['screen_name']}\" target=\"_blank\">{$value['screen_name']}</a></td>
       <td><a href=\"https://twitter.com/{$value['screen_name']}\" target=\"_blank\">{$value['user_id']}</a></td>
-      <td>{$value['last_ip']}</td>
+      <td style=\"background-color:". stringToColor($value['last_ip']) ."\">{$value['last_ip']}</td>
       <td>{$value['created_on']}</td>
       <td>{$value['last_updated']}</td>
       <td>{$value['tracery_size']}</td>
